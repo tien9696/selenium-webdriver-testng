409d90deb8f83cbb7c8745597305db8267366616
Themf_000 <Themf_000@192.168.1.9>
2021-10-21 16:24:48 +0700

    Create new Repository

---
+++ javaTester/javabasic/topic_01_System_Properties.java
@@ -0,0 +1,18 @@
+package javabasic;
+
+public class topic_01_System_Properties {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+		System.out.println(projectPath + "\\browserDrivers\\geckodriver.exe");
+
+		//C:\Users\Themf_000\Downloads\eclipse-java-2021-03-R-win32-x86_64\Workspace\selenium-webdriver-testng
+//C:\Users\Themf_000\Downloads\eclipse-java-2021-03-R-win32-x86_64\Workspace\selenium-webdriver-testng\\browserDrivers\\geckodriver.exe
+//C:\Users\Themf_000\Downloads\eclipse-java-2021-03-R-win32-x86_64\Workspace\selenium-webdriver-testng\browserDrivers\geckodriver.exe
+//C:\Users\Themf_000\Downloads\eclipse-java-2021-03-R-win32-x86_64\Workspace\selenium-webdriver-testng\browserDrivers\geckodriver.exe
+
+	}
+
+}
